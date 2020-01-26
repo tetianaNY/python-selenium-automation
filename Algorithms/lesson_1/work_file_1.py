64108b27d6248a68cfe56c3972db3b91f1e05b2f
Tania <klats.usa@gmail.com>
2020-01-26 13:10:15 -0500

    Lesson 1

---
+++ Algorithms/lesson_1/work_file_1.py
@@ -0,0 +1,22 @@
+array = [1,5,7,3,4,5,6,22]
+
+# FOR LOOPS
+#
+# for item in array:
+#     print(item)
+
+
+# FOR Range
+# for item in range(len(array)):
+#     print(array[item])
+
+# WHILE
+# index = 0
+index = len(array)-1
+while index >= 0:
+    print(array[index])
+    index -= 1
+    # index += 1
+# else:
+#     print('empty array')
+
