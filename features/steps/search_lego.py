e4b00afe41b3a04a6e41299b7f946f30f7b4b9de
Tania <klats.usa@gmail.com>
2020-02-01 13:25:43 -0500

    Classwork

--- features/steps/search_lego.py
+++ features/steps/search_lego.py
@@ -3,7 +3,8 @@ from behave import given, when, then
 from time import sleep
 
 SEARCH_INPUT_LOCATOR = (By.ID, 'twotabsearchtextbox')
-SEARCH_BUTTON_LOCATOR = (By.CSS_SELECTOR, 'input.nav-input[@value="go"]')
+SEARCH_BUTTON_LOCATOR = (By.CSS_SELECTOR, 'input.nav-input[value="Go"]')
+HEADER_GOODS_PAGE_LOCATOR = (By.XPATH, "//div[@id='search']//div[@class='a-section a-spacing-small a-spacing-top-small']//span[@class='a-color-state a-text-bold']")
 
 @given('Open Amazon main page')
 def open_amazon_page(context):
@@ -13,15 +14,15 @@ def open_amazon_page(context):
 def search_input_fill(context, search_text):
     amazon_search_input = context.driver.find_element(*SEARCH_INPUT_LOCATOR)
     amazon_search_input.clear()
-    amazon_search_input.send_keys('Lego')
+    amazon_search_input.send_keys(search_text)
     sleep(4)
 
 @when('Click on search button')
 def click_search_button(context):
-    amazon_seach_button = context.driver.find_element(*SEARCH_BUTTON_LOCATOR)
-    amazon_seach_button.click
+    amazon_search_button = context.driver.find_element(*SEARCH_BUTTON_LOCATOR)
+    amazon_search_button.click()
     sleep(2)
 
 @then('Assert {search_text} in header on the page')
 def check_header_goods_page(context, search_text):
-    assert 'Lego' in context.driver.find_element(*HEADER_GOODS_PAGE_LOCATOR).text, f'Expected "Lego"
+    assert 'lego' in context.driver.find_element(*HEADER_GOODS_PAGE_LOCATOR).text, f'Expected "lego", but got {search_text}'
