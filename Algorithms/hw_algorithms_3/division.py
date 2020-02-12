cd5c49bcf79b19af63b34def618f1b2cda184d25
Tania <klats.usa@gmail.com>
2020-02-12 16:44:52 -0500

    left to make an interruption

---
+++ Algorithms/hw_algorithms_3/division.py
@@ -0,0 +1,50 @@
+
+
+def devision(num1, num2, accur):
+    count = 0
+    result = ''
+    while num1 >= num2:
+        num1 = num1 - num2
+        count += 1
+    if num1 == 0:
+        result = str(count)
+        result += '.0'
+        return result
+    i = 0
+    float_result = []
+    num1_2 = num1
+
+# сколько знаков после запятой
+    for i in range(accur):
+      float_index = 0
+      count2 = 0
+# умножаем на 10 для вычитания, сколько раз умножили столько раз
+
+      while num1_2 <= num2:
+          num1_2 *= 10
+          float_index += 1
+
+# добавляем 0, после запятой, если нет возможности поделить на цело
+      q = 1
+      if float_index > 1:
+        for q in range(float_index-1):
+          float_result.append('0')
+
+
+# вычитаем и записываем в список количество вычитаний на этом десятке
+
+      while num1_2 >= num2:
+          num1_2 = num1_2 - num2
+          count2 += 1
+      # print(count2)
+      float_result.append(str(count2))
+
+    print(float_result)
+    result = str(count)
+    result += '.'
+    result += ''.join(float_result)
+
+    return result
+
+print("The total is", devision(1000, 178, 20))
+
