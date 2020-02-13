28b87b1a02b902f236ba3941d4be73742c8e56cc
Tania <klats.usa@gmail.com>
2020-02-13 09:48:44 -0500

    Accurancy corrected with zero's

--- Algorithms/hw_algorithms_3/division.py
+++ Algorithms/hw_algorithms_3/division.py
@@ -12,32 +12,37 @@ def devision(num1, num2, accur):
         count += 1
     if num1 == 0:
         result = str(count)
-        result += '.0'
+        result += '.00'
         return result
     i = 0
     float_result = []
     num1_2 = num1
+    count_0 = 0
 
 # сколько знаков после запятой
     for i in range(accur):
       float_index = 0
       count2 = 0
+      if num1_2 == 0: break
 
-# умножаем на 10 для вычитания
+# Так как у нас десятичная система счисления, умножаем на 10 для вычитания. Каждая 10 это 1 разряд.
       while num1_2 <= num2:
           num1_2 *= 10
           float_index += 1
           if num1_2 == 0: break
 
-# добавляем 0, после запятой, если нет возможности поделить на цело
+# добавляем 0, после запятой, если нет возможности поделить нАцело. (Вычесть с положительным остатком)
       q = 1
       if float_index > 1:
         for q in range(float_index-1):
           float_result.append('0')
+          count_0 += 1
 
 
 
-# вычитаем и записываем в список количество вычитаний на этом десятке
+# вычитаем и записываем в список количество вычитаний на этом десятке, также как в начале получали целое число
+      # только для каждого разряда после запятой
+
       while num1_2 >= num2:
           num1_2 = num1_2 - num2
           count2 += 1
@@ -47,7 +52,7 @@ def devision(num1, num2, accur):
     result += '.'
     result += ''.join(float_result)
 
-    return result
+    return result[:len(result)-count_0]
 
-print("The total is", devision(1, 1777, 100))
+print("The total is", devision(1, 50, 100))
 
