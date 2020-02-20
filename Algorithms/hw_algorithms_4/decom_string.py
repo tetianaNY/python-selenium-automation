12307541df026707903e2ab10fa9505b94f3abf8
Tania <klats.usa@gmail.com>
2020-02-19 21:06:03 -0500

    works, but not good enough

--- Algorithms/hw_algorithms_4/decom_string.py
+++ Algorithms/hw_algorithms_4/decom_string.py
@@ -1,19 +1,23 @@
+# работает только с текущим примером, может работать с любым примером в формате x0y0z0 ... n0 где xyzn буквы,
+# 0 - одно разрядное число
+# Завтра подумаю еще.
+
 def decompression(comp_string):
     i = 0
     decom_list = []
     decom_string = comp_string[::2]
-    print(decom_string)
+    # print(decom_string)
     for i in range(len(comp_string)):
         if comp_string[i] not in decom_string:
-            print(comp_string[i])
+            # print(comp_string[i])
             decom_list.append(int(comp_string[i]))
 
     new_string = ''
     for i in range(len(comp_string)//2):
         new_string += decom_string[i] * decom_list[i]
-        
+
     return new_string + comp_string[-1]
 
 
 
-print(decompression('а3б5в5г'))
+print(decompression('а4б3в2г'))
