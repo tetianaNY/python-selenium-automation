7b351047fb514b1bebe91adf25e01ab91df4edad
Tania <klats.usa@gmail.com>
2020-02-26 10:39:10 -0500

    .strip() removed

--- Algorithms/hw_algorithms_5/spaces_remove.py
+++ Algorithms/hw_algorithms_5/spaces_remove.py
@@ -4,7 +4,7 @@ def spaces_remove(str):
     str_list1 = str.split(' ')
     for i in range(len(str_list1)):
         if str_list1[i] == '': continue
-        str_list2.append(str_list1[i].strip())
+        str_list2.append(str_list1[i])
     return ' '.join(str_list2)
 
-print(spaces_remove('  sdfsdf df     dfsfsdfsdf ff dsfsd  dsadfasds       dsd    '))
+print(spaces_remove('  sdfsdf df  ! ! hjk y6    dfsfsdfsdf ff dsfsd  dsadfasds       dsd    '))
