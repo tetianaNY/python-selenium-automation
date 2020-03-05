bb5f8992a20c9b570650cb44fb2f9f3e857e88d3
Tania <klats.usa@gmail.com>
2020-03-05 13:01:55 -0500

    For first and second task

--- Homework_week_7/pages/main_page.py
+++ Homework_week_7/pages/main_page.py
@@ -3,6 +3,7 @@ from Homework_week_7.pages.base_page import Page
 
 class MainPage(Page):
     ORDERS_LOCATOR = (By.CSS_SELECTOR, '#nav-orders .nav-line-2')
+    CART_LOCATOR = (By.CSS_SELECTOR, '#nav-cart')
 
 
     def open(self):
@@ -10,6 +11,9 @@ class MainPage(Page):
 
     def open_orders(self):
         self.click(*self.ORDERS_LOCATOR)
+
+    def open_cart(self):
+        self.click(*self.CART_LOCATOR)
 
 
 
