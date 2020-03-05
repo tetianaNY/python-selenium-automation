9916e17ba267d53477d7915294e19dd2007d0e09
Tania <klats.usa@gmail.com>
2020-03-05 14:57:07 -0500

    application.py

---
+++ lesson_7/app/application.py
@@ -0,0 +1,9 @@
+from lesson_7.pages.main_page import MainPage
+from lesson_7.pages.results_page import ResultsPage
+
+class Application:
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.main_page = MainPage(self.driver)
+        self.results_page = ResultsPage(self.driver)
