4dbdab4aa25b19414822dff6f639727677c37d35
Tania <klats.usa@gmail.com>
2020-03-05 13:02:02 -0500

    For first and second task

--- Homework_week_7/pages/results_page.py
+++ Homework_week_7/pages/results_page.py
@@ -3,8 +3,12 @@ from Homework_week_7.pages.base_page import Page
 
 class ResultsPage(Page):
     SIGN_IN_H1 = (By.CSS_SELECTOR, 'h1.a-spacing-small')
+    EMPTY_CART = (By.CSS_SELECTOR, '.sc-your-amazon-cart-is-empty')
 
     def verify_header_result(self, text):
         self.verify_elmnt_txt(text, *self.SIGN_IN_H1)
+
+    def verify_empty_cart(self, text):
+        self.verify_elmnt_txt(text, *self.EMPTY_CART)
 
 
