d8a13abe1097146bd774ec497efa96a821f48d5e
Tania <klats.usa@gmail.com>
2020-03-08 19:35:19 -0400

    for graduated work

---
+++ Graduated_work_HomeDepot/pages/main_page.py
@@ -0,0 +1,53 @@
+from selenium.webdriver.common.by import By
+from Graduated_work_HomeDepot.pages.base_page import Page
+from time import sleep
+
+class MainPage(Page):
+    SEARCH_BOX_LOCATOR = (By.CSS_SELECTOR, '#headerSearch')
+    SEARCH_BUTTON_LOCATOR = (By.CSS_SELECTOR, '#headerSearchButton')
+    ACCOUNT_BUTTON_LOCATOR = (By.CSS_SELECTOR, '#headerMyAccount')
+    REGISTER_BUTTON_LOCATOR = (By.CSS_SELECTOR, 'a.bttn-outline .bttn__content')
+    SIGN_IN_BUTTON_LOCATOR = (By.CSS_SELECTOR, '#SPSOSignIn a.bttn--primary .bttn__content')
+    SUGGESTIONS_CONTAINER = (By.CSS_SELECTOR, '#typeAheadFlyOut a')
+    SUGGESTIONS_LOCATOR = (By.CSS_SELECTOR, '#typeAheadFlyOut a:not(.categories)')
+    CART_BUTTON = (By.CSS_SELECTOR, '#headerCart')
+
+    def open_p(self):
+        self.open_page('https://www.homedepot.com')
+
+    def search_product_noclick(self, text: str):
+        self.input(text, *self.SEARCH_BOX_LOCATOR)
+
+    def search_field_click(self):
+        self.click(*self.SEARCH_BOX_LOCATOR)
+
+    def search_button_click(self):
+        self.click(*self.SEARCH_BUTTON_LOCATOR)
+
+    def account_button_click(self):
+        self.click(*self.ACCOUNT_BUTTON_LOCATOR)
+
+    def register_button_click(self):
+        self.wait_for_element_appear(self.REGISTER_BUTTON_LOCATOR)
+        self.click(*self.REGISTER_BUTTON_LOCATOR)
+
+    def sign_in_button_click(self):
+        self.wait_for_element_appear(self.SIGN_IN_BUTTON_LOCATOR)
+        self.click(*self.SIGN_IN_BUTTON_LOCATOR)
+
+    def cart_button_click(self):
+        self.wait_for_element_appear(self.CART_BUTTON)
+        self.click(*self.CART_BUTTON)
+
+    def counting_result(self, text: str):
+        self.wait_for_element_appear(self.SUGGESTIONS_CONTAINER)
+        self.count_suggestion_items(text, *self.SUGGESTIONS_LOCATOR)
+
+
+
+
+
+
+
+
+
