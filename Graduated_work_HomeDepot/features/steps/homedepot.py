24ac11e09588607fc15e5d0dd527585c399ecfec
Tania <klats.usa@gmail.com>
2020-03-09 11:26:08 -0400

    Done

--- Graduated_work_HomeDepot/features/steps/homedepot.py
+++ Graduated_work_HomeDepot/features/steps/homedepot.py
@@ -110,6 +110,27 @@ def new_search_result(context):
 def second_result_verification(context):
     context.app.results_page.item_in_the_cart_result('2')
 
+@then('Click cart button')
+def result_page_cart(context):
+    context.app.results_page.click_cart_button()
+
+@then('Change quantity from 1 to 2')
+def change_quantity(context):
+    context.app.results_page.change_quantity('2')
+
+@then('Expected items in cart will be 2')
+def result_verification2(context):
+    context.app.results_page.item_in_the_cart_result('2')
+    sleep(2)
+
+@then('Expected product has "write review" button')
+def result_verification3(context):
+    context.app.results_page.review_button_check()
+
+
+
+
+
 
 
 
