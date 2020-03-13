a14168dd2a4241ab5c477258f745ce7e0c681b07
Tania <klats.usa@gmail.com>
2020-03-13 12:16:01 -0400

    lesson 9 enviromental

---
+++ Classwork/lesson_9/features/environment.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from Classwork.lesson_9.app.application import Application
+
+
+def browser_init(context):
+    """
+    :param context: Behave context
+    """
+    context.driver = webdriver.Chrome()
+    # context.browser = webdriver.Safari()
+    # context.browser = webdriver.Firefox()
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(4)
+    context.app = Application(context.driver)
+    context.wait = WebDriverWait(context.driver, 15)
+
+
+def before_scenario(context, scenario):
+    print('\nStarted scenario: ', scenario.name)
+    browser_init(context)
+
+
+def before_step(context, step):
+    print('\nStarted step: ', step)
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        print('\nStep failed: ', step)
+
+
+def after_scenario(context, feature):
+    context.driver.delete_all_cookies()
+    context.driver.quit()
