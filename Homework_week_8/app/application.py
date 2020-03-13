0710544ff1db125a0397a55a2668a1cf104cd0ba
Tania <klats.usa@gmail.com>
2020-03-13 13:17:11 -0400

    product page added

--- Homework_week_8/app/application.py
+++ Homework_week_8/app/application.py
@@ -1,5 +1,6 @@
 from Homework_week_8.pages.main_page import MainPage
 from Homework_week_8.pages.results_page import ResultsPage
+from Homework_week_8.pages.product_page import ProductPage
 
 class Application:
     def __init__(self, driver):
@@ -7,3 +8,4 @@ class Application:
 
         self.main_page = MainPage(self.driver)
         self.results_page = ResultsPage(self.driver)
+        self.product_page = ProductPage(self.driver)
